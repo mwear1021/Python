90b23b4929e250ded3c5f58875d1d68735508e94
mwear1021 <mwear1021@gmail.com>
2024-12-02 19:06:25 -0600

    initial commit of countdown GUI

---
+++ countdown_to_date_GUI.py
@@ -0,0 +1,38 @@
+import datetime
+import PySimpleGUI as sg
+
+def countdown_window(target_date):
+    '''creates the countdown window'''
+
+    layout = [
+        [sg.Text('Countdown to target date: ', font=('Helvetica', 20), text_color='White', background_color="Black")],
+        [sg.Text("", size=(20, 2), font=('Helvetica', 40), key="-TIME-", text_color="White", background_color="Black")],
+        [sg.Button('Exit')]
+    ]
+
+    window = sg.Window("Countdown", layout, background_color="Black")
+
+    while True:
+        event, _ = window.read(timeout=1000)   #update every second
+
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+
+        time_left = target_date - datetime.datetime.today()
+
+        if time_left.total_seconds() <= 0:
+            window["-TIME-"].update("Time's Up!")
+        else:
+            window["-TIME-"].update(str(time_left).split('.')[0])   # remove milliseconds
+
+    window.close()
+
+if __name__ == '__main__':
+    year = int(input("Enter target year: "))
+    month = int(input("Enter target month: "))
+    day = int(input("Enter target day: "))
+    hour = int(input("Enter target hour: "))
+    minute = int(input("Enter target minute: "))
+
+    target_date = datetime.datetime(year, month, day, hour, minute)
+    countdown_window(target_date)
